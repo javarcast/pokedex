4132940f75ca098d16e72df81f885d7bca43ad8a
Jesus Varela <javarcast93@gmail.com>
2022-02-02 13:17:16 -0400

    add link in logo

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,9 +1,10 @@
 <template>
   <header class="h-20 shadow-lg px-4 w-full bg-red-600 flex justify-center items-center">
-    <div class="flex items-center text-white">
+    <router-link to="/" class="flex items-center flex-center text-white">
+
       <img class="h-14" alt="Pokeball logo" src="@/assets/img/logo-1.png">
       <h1 class="text-3xl ml-4">PokeApi</h1>
-    </div>
+    </router-link>
 
   </header>
 </template>
