c8bfa3bd0822bd82b6dee252c26133428a392b61
Jesus Varela <javarcast93@gmail.com>
2022-02-03 17:34:58 -0400

    add name in footer

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class=" flex items-center justify-center z-30 bottom-0 w-full h-14 px-4 bg-red-600">
-    <h1 class="text-black-300">footer</h1>
+    <h2 class="text-white"><a href="https://github.com/javarcast">Varcast</a></h2>
   </footer>
 </template>
 <script>
