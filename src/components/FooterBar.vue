a26113f66fbf509b535d397ab51e6fd4b71eafd3
Jesus Varela <javarcast93@gmail.com>
2022-02-03 21:31:50 -0400

    add logo and font

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,6 +1,9 @@
 <template>
   <footer class=" flex items-center justify-center z-30 bottom-0 w-full h-14 px-4 bg-red-600">
-    <h2 class="text-white"><a href="https://github.com/javarcast">Varcast</a></h2>
+    <a class="flex  justify-center items-center cubic" href="https://github.com/javarcast">
+      <img class="h-10" src="@/assets/img/varcast.png" alt="varcast">
+      <h2 class="text-white ml-2 shadow-lg">Varcast</h2>
+    </a>
   </footer>
 </template>
 <script>
@@ -9,4 +12,9 @@ export default {
 };
 </script>
 <style scoped>
+  .cubic {
+    font-family: "cubic", serif;
+    letter-spacing: 10px;
+  }
+
 </style>
